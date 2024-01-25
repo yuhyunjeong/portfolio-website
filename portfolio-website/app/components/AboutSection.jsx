1b3483323f23769a825ccfe406c8b1593f383a2b
jade <jadejung95@gmail.com>
2024-01-25 22:27:03 +0900

    About Section

--- portfolio-website/app/components/AboutSection.jsx
+++ portfolio-website/app/components/AboutSection.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+"use client";
+import React, { useTransition, useState } from "react";
 
 const AboutSection = () => {
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <h1>About me</h1>
-        <p>
+        <h1 className="text-4xl font-bold mb-4">About me</h1>
+        <p className="text-base lg:text-lg">
           I have experience with full-stack development using React,
           JavaScript(JQuery), HTML5, CSS, Bootstrap on the frontend and Java,
           Spring(boot), MySQL, Oracle on the backend. Other skills include
@@ -13,6 +14,13 @@ const AboutSection = () => {
           good front-end developer who can consider both users and back-end
           developers.
         </p>
+        <div className="flex flex-row mt-8">
+          <span className="mr-3 font-semibold text-slate-200 hover:text-white border-b">
+            Skills
+          </span>
+          <span>Education</span>
+          <span>Certification</span>
+        </div>
       </div>
     </section>
   );
