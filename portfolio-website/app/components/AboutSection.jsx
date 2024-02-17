68286a8a2e66bd367dcbe1c4da0d7c6622f2c104
jade <jadejung95@gmail.com>
2024-02-17 23:58:20 +0900

    AboutSection

--- portfolio-website/app/components/AboutSection.jsx
+++ portfolio-website/app/components/AboutSection.jsx
@@ -59,36 +59,44 @@ const AboutSection = () => {
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <h1 className="text-4xl font-bold mb-4">About me</h1>
-        <p className="text-base lg:text-lg">
-          I have experience with full-stack development using React,
-          JavaScript(JQuery), HTML5, CSS, Bootstrap on the frontend and Java,
-          Spring(boot), MySQL, Oracle on the backend. Other skills include
-          experience with Docker, Linux and a little bit of Matlab. I can be a
-          good front-end developer who can consider both users and back-end
-          developers.
-        </p>
-        <div className="flex flex-row mt-8">
-          <TabButton
-            selectTab={() => handleTabChange("skills")}
-            active={tab === "skills"}
-          >
-            Skills
-          </TabButton>
-          <TabButton
-            selectTab={() => handleTabChange("education")}
-            active={tab === "education"}
-          >
-            Education
-          </TabButton>
-          <TabButton
-            selectTab={() => handleTabChange("certification")}
-            active={tab === "certification"}
-          >
-            Certification
-          </TabButton>
+        <div className="mt-4 md:t-0 text-left flex flex-col h-full">
+          <h1 className="text-4xl font-bold mb-4">About me</h1>
+          <p className="text-base lg:text-lg">
+            Hi! 👋 I'm a junior web developer with a focus on Vanilla
+            JavaScript, CSS, and I'm diving into React.js. I'm moving to
+            Toronto, Canada 🇨🇦 with a 2-year work permit soon. <br></br>I have
+            experience with full-stack development using JavaScript, HTML5, CSS,
+            and Bootstrap on the frontend, and Java, Spring(boot), MySQL, and
+            Oracle on the backend. My experience extends to Docker, Linux, and a
+            bit of Matlab. <br></br>
+            Currently passionate about elevating my skills as a Front-end
+            developer, I aim to blend functionality with eye-catching UX design.
+            👩🏻‍💻
+          </p>
+          <div className="flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certification")}
+              active={tab === "certification"}
+            >
+              Certification
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
         </div>
-        <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
       </div>
     </section>
   );
