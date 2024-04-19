18edca0a0a740a87209ccb11c2c9d5d233ae3857
jade <jadejung95@gmail.com>
2024-04-19 14:32:40 +0900

    card hover

--- portfolio-website/app/components/ProjectCard.jsx
+++ portfolio-website/app/components/ProjectCard.jsx
@@ -4,11 +4,13 @@ const ProjectCard = ({ imgUrl, title, description }) => {
   return (
     <div>
       <div
-        className="h-52 md:h-72 rounded-t-xl"
+        className="h-52 md:h-72 rounded-t-xl relative group"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      ></div>
-      <div className="text-white">
-        <h5 className="text-xl font-semibold">{title}</h5>
+      >
+        <div className="overlay absolute items-center justify-center top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 "></div>
+      </div>
+      <div className="text-white rounded-b-xl px-4 py-6">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-slate-200">{description}</p>
       </div>
     </div>
