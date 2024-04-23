66d7488aa381ae95a15bd9342b3e87fee9ef08c6
jade <jadejung95@gmail.com>
2024-04-24 08:11:13 +0900

    message box

--- portfolio-website/app/components/EmailSection.jsx
+++ portfolio-website/app/components/EmailSection.jsx
@@ -29,16 +29,58 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <form className="flex flex-col gap-4">
-          <label htmlFor="email" className="text-white">
-            Your Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            required
-            placeholder="yuhyunjungdev@gmail.com"
-          />
+        <form className="flex flex-col gap-6">
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="text-white block mb-2 text-sm font-medium"
+            >
+              Your Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              required
+              className="bg-[#eeecefd9] border border-[#e8cbee97] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="yuhyunjungdev@gmail.com"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="subject"
+              className="text-white block mb-2 text-sm font-medium"
+            >
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              required
+              className="bg-[#eeecefd9] border border-[#e8cbee97] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Just say hi!"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="message"
+              className="text-white block mb-2 text-sm font-medium"
+            >
+              Message
+            </label>
+            <textarea
+              type="text"
+              id="message"
+              required
+              className="bg-[#eeecefd9] border border-[#e8cbee97] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Let's talk about ..."
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-gray-400 border border-[#decde1] hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+          >
+            Send Message
+          </button>
         </form>
       </div>
     </section>
