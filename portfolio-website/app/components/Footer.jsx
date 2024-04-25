6b661bc16420888df970725bccd0e9dadfd9d337
jade <jadejung95@gmail.com>
2024-04-25 09:50:24 +0900

    footer

---
+++ portfolio-website/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border border-t-slate-200 border-l-transparent border-r-transparent text-slate-200">
+      <div className="container p-12 flex justify-between">
+        <span>LOGO</span>
+        <p>All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
