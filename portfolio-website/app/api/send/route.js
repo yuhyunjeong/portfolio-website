1beca7ffbf0a902b7494f6d7df863bf205063617
jade <jadejung95@gmail.com>
2024-05-01 13:24:52 +0900

    comment

--- portfolio-website/app/api/send/route.js
+++ portfolio-website/app/api/send/route.js
@@ -1,14 +1,18 @@
 //import { EmailTemplate } from "../../../components/EmailTemplate";
 import { Resend } from "resend";
 
+// creates a new instance of the Resend class, passing the Resend API key obtained from the environment variables.
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
+// exports an asynchronous function named POST, which handles HTTP POST requests
 export async function POST(req, res) {
+  // JSON data sent in the request body and extracts the email, subject, and message fields.
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
 
   try {
+    // sends an email using the Resend API's send method
     const data = await resend.emails.send({
       from: fromEmail,
       to: email,
