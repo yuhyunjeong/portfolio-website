e7fdbc250a9f53a43c6c5a63f1dd73781f344e92
jade <jadejung95@gmail.com>
2024-09-03 02:10:18 +0900

    tab button motion

--- portfolio-website/app/components/TabButton.jsx
+++ portfolio-website/app/components/TabButton.jsx
@@ -1,14 +1,26 @@
+import { motion } from "framer-motion";
 import React from "react";
 
+const variants = {
+  default: { width: 0 },
+  active: { width: "calc(100% - 0.75rem)" },
+};
+
 const TabButton = ({ active, selectTab, children }) => {
-  const buttonClasses = active
-    ? "text-white border-b-2 border-teal-100"
-    : "text-slate-200";
+  const buttonClasses = active ? "text-white" : "text-slate-200";
   return (
-    <button onClick={selectTab}>
-      <p className={`mr-3 !font-semibold hover:text-white ${buttonClasses}`}>
+    <button onClick={selectTab} className="relative overflow-hidden">
+      <p
+        className={`mr-3 !font-semibold hover:text-white transition-colors ${buttonClasses}`}
+      >
         {children}
       </p>
+      <motion.div
+        animate={active ? "active" : "default"}
+        variants={variants}
+        className=" bg-teal-100 mr-3 rounded-full"
+        style={{ height: "2px" }}
+      ></motion.div>
     </button>
   );
 };
