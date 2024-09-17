cd63a45b4c7f0f659d6a1574c729352d626dc8c0
jade <jadejung95@gmail.com>
2024-09-17 11:12:31 +0900

    unoptimized

--- portfolio-website/next.config.js
+++ portfolio-website/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // 필요한 설정을 추가
+    unoptimized: true, // 이미지 최적화를 비활성화할 수 있습니다. 문제 해결에 도움될 수 있음.
+  },
+};
 
 module.exports = nextConfig;
